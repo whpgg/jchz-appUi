a96a72f29ba516a18df22b6075149b2fd913aaed
wanghuapeng <895517489@qq.com>
2019-10-10 14:18:22 +0800

    进行简单测试

--- src/app.js
+++ src/app.js
@@ -1,13 +1,94 @@
-import Vue from 'vue'
-import Button from './button'
-import Icon from './icon'
-import ButtonGroup from './button-group'
-Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
-Vue.component('g-button-group', ButtonGroup)
+import Vue from 'vue';
+import Button from './button';
+import Icon from './icon';
+import ButtonGroup from './button-group';
+Vue.component('g-button', Button);
+Vue.component('g-icon', Icon);
+Vue.component('g-button-group', ButtonGroup);
 new Vue({
-    el:'#app',
-    data:{
-        loading1:true
+    el: '#app',
+    data: {
+        loading1: true
     }
-})
+});
+import chai from 'chai';
+
+const expect = chai.expect;
+//测试
+{
+    const constructor = Vue.extend(Button);
+    const button = new constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    });
+    button.$mount();
+    let useElement = button.$el.querySelector('use');
+    expect(useElement.getAttribute('xlink:href')).to.eq('#isetting');
+    button.$el.remove();
+    button.$destroy();
+}
+
+{
+    const constructor = Vue.extend(Button);
+    const button = new constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    });
+    button.$mount();
+    let useElement = button.$el.querySelector('use');
+    expect(useElement.getAttribute('xlink:href')).to.eq('#iLoading');
+    button.$el.remove();
+    button.$destroy();
+}
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const constructor = Vue.extend(Button);
+    const button = new constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true
+        }
+    });
+    button.$mount(div);
+    let svg = button.$el.querySelector('svg');
+    let { order } = window.getComputedStyle(svg);
+    expect(order).to.eq('1');
+    button.$el.remove();
+    button.$destroy();
+}
+
+{
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    const constructor = Vue.extend(Button);
+    const button = new constructor({
+        propsData: {
+            icon: 'setting',
+            loading: true,
+            iconPosition: 'right'
+        }
+    });
+    button.$mount(div);
+    let svg = button.$el.querySelector('svg');
+    let { order } = window.getComputedStyle(svg);
+    expect(order).to.eq('2');
+    button.$el.remove();
+    button.$destroy();
+}
+{
+    const constructor = Vue.extend(Button);
+    const gButton = new constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    });
+    gButton.$mount();
+    gButton.$on('click',function(){
+        console.log(1)
+    })
+    gButton.$el.click();
+}
