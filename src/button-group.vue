46584ed36d3ab5eb364031d59acddc15eada9bdb
wanghuapeng <895517489@qq.com>
2019-10-10 10:07:42 +0800

    修改button1像素bug

--- src/button-group.vue
+++ src/button-group.vue
@@ -4,22 +4,32 @@
     </div>
 </template>
 <script>
-export default {};
+export default {
+    mounted() {
+        for (let node of this.$el.children) {
+            if (node.nodeName.toLowerCase() !== "button") {
+                console.warn("g-button-group的子元素应该全是 g-button");
+            }
+        }
+    }
+};
 </script>
 <style lang="scss">
-.g-button-group{
+.g-button-group {
     display: inline-flex;
     vertical-align: middle;
     > .g-button {
         border-radius: 0;
-        margin-left: -1px;
-        &:first-child{
+        &:not(:first-child) {
+            margin-left: -1px;
+        }
+        &:first-child {
             border-top-left-radius: var(--border-radius);
             border-bottom-left-radius: var(--border-radius);
         }
-        &:last-child{
+        &:last-child {
             border-top-right-radius: var(--border-radius);
-            border-bottom-right-radius: var(--border-radius); 
+            border-bottom-right-radius: var(--border-radius);
         }
         &:hover {
             position: relative;
