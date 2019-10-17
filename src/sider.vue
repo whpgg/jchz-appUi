2416246106cbea64a97210e956beddf1b2fb6bd8
wanghuapeng <895517489@qq.com>
2019-10-17 18:24:34 +0800

    默认布局基本功能完成

---
+++ src/sider.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="sider">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: "jcSider"
+};
+</script>
+<style lang="scss" scoped>
+.sider {
+}
+</style>
