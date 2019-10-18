2c933757e3ec0d75ca07427172b3311b93006f92
wanghuapeng <895517489@qq.com>
2019-10-18 15:15:33 +0800

    提交tost

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ import Header from './header';
 import Sider from './sider';
 import Content from './content';
 import Footer from './footer';
+import Toast from './toast';
+import plugin from './plugin'
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
 Vue.component('g-button-group', ButtonGroup);
@@ -21,14 +23,18 @@ Vue.component('g-header',Header);
 Vue.component('g-sider',Sider);
 Vue.component('g-content',Content);
 Vue.component('g-footer',Footer);
+Vue.component('g-toast',Toast);
+Vue.use(plugin);
 new Vue({
     el: '#app',
     data: {
         loading1: true
     },
+    mounted(){
+    },
     methods: {
-        inputChange(e) {
-            console.log('111111', e.target.value);
+        showToast(){
+            this.$toast('我是message')
         }
     }
 });
---
+++ src/toast.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="toast">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    name: "jcToast"
+};
+</script>
+<style lang="scss" scoped>
+$font-size: 14px;
+$toast-height: 40px;
+$toast-bg: rgba(0,0,0,0.75);
+.toast {
+    font-size: $font-size;
+    line-height: 1.8;
+    height: $toast-height;
+    position: fixed;
+    color: #ffffff;
+    padding: 0 16px;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%);
+    display: flex;
+    align-items: center;
+    border-radius: 4px;
+    background: $toast-bg;
+    box-shadow: 0 0 3px 0 rgba(0,0,0,0.50)
+}
+</style>
