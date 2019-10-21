227afc9aa50a1fadb1aaf7b2ad011a153c943d26
wanghuapeng <895517489@qq.com>
2019-10-21 18:19:25 +0800

    提交topst测试

--- src/toast.vue
+++ src/toast.vue
@@ -33,12 +33,11 @@ export default {
     name: "jcToast",
     props: {
         autoClose: {
-            type: Boolean,
-            default: true
-        },
-        autoCloseDelay: {
-            type: Number,
-            default: 3
+            type: [Boolean, Number],
+            default: 3,
+            validator (value){
+                return value === false || typeof value === 'number'
+            }
         },
         closeButton: {
             type: Object,
@@ -82,7 +81,7 @@ export default {
             if (this.autoClose) {
                 setTimeout(() => {
                     this.close();
-                }, this.autoCloseDelay * 1000);
+                }, this.autoClose * 1000);
             }
         },
         close() {
