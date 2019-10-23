c553e4eb0b762ca6061ca13de033c131df1ae6f7
wanghuapeng <895517489@qq.com>
2019-10-23 10:17:18 +0800

    完成tab切换

--- src/tabs-nav.vue
+++ src/tabs-nav.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="tabs-nav">
         <slot></slot>
+        <div class="line" ref="line"></div>
         <div class="actionsWrapper">
             <slot name="actions"></slot>
         </div>
@@ -9,17 +10,32 @@
 <script>
 export default {
     name: "jcTabsNav",
-    inject: ["eventBus"]
+    inject: ["eventBus"],
+    mounted (){
+        this.eventBus.$on('update:selected',(item, vm)=>{
+            let {width,height,top,left} = vm.$el.getBoundingClientRect();
+            this.$refs.line.style.width = `${width}px`;
+            this.$refs.line.style.transform = `translateX(${left}px)`;
+        })
+    }
 };
 </script>
 <style lang="scss" scoped>
 $tab-height: 40px;
+$blue: blue;
 .tabs-nav {
     display: flex;
     height: $tab-height;
     justify-content: flex-start;
     align-items: center;
     border: 1px solid red;
+    position: relative;
+    > .line{
+        position: absolute;
+        bottom: 0;
+        border-bottom: 3px solid $blue;
+        transition: all 250ms;
+    }
     > .actionsWrapper{
         margin-left: auto;
     }
