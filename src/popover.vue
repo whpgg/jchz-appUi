508a96644d686014f97f6bc596b3a7070dd49f9f
wanghuapeng <895517489@qq.com>
2019-10-24 11:37:02 +0800

    修改当元素为overflowhidden时出现的不能显示问题

--- src/popover.vue
+++ src/popover.vue
@@ -4,16 +4,15 @@
         @click.stop="xxx"
     >
         <div
+            ref="contentWrapper"
             class="content-wrapper"
             v-if="visible"
-            @click.stop
         >
-            <slot
-                name="content"
-                v-if="visible"
-            ></slot>
+            <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 <script>
@@ -24,17 +23,24 @@ export default {
             visible: false
         };
     },
+    mounted() {
+        console.log(this.$refs.triggerWrapper);
+    },
     methods: {
         xxx() {
-            this.visible = !this.visible;   
-            if(this.visible === true){
-                this.$nextTick(()=>{
+            this.visible = !this.visible;
+            if (this.visible === true) {
+                this.$nextTick(() => {
+                    document.body.appendChild(this.$refs.contentWrapper);
+                    let {left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
+                    this.$refs.contentWrapper.style.left = left + 'px';
+                    this.$refs.contentWrapper.style.top = top + 'px';
                     let eventHandler = () => {
-                        this.visible = false
-                        document.removeEventListener('click',eventHandler)
-                    }
-                    document.addEventListener('click', eventHandler)
-                })
+                        this.visible = false;
+                        document.removeEventListener("click", eventHandler);
+                    };
+                    document.addEventListener("click", eventHandler);
+                });
             }
         }
     }
@@ -45,12 +51,11 @@ export default {
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-        position: absolute;
-        bottom: 100%;
-        left: 0;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    }
+}
+.content-wrapper {
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    transform: translateY(-100%);
 }
 </style>
