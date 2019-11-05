18bbc0a4de00332a182a1ea11ef4abd1ca477d9d
wanghuapeng <895517489@qq.com>
2019-11-05 10:23:48 +0800

    递归子选项

--- src/app.js
+++ src/app.js
@@ -1,52 +1,79 @@
-import Vue from 'vue';
 import Button from './button';
-import Icon from './icon';
 import ButtonGroup from './button-group';
-import Input from './input';
-import Row from './row';
+import Cascader from './cascader';
 import Col from './col';
-import Layout from './layout';
-import Header from './header';
-import Sider from './sider';
 import Content from './content';
 import Footer from './footer';
-import Toast from './toast';
-import plugin from './plugin';
+import Header from './header';
+import Icon from './icon';
+import Input from './input';
+import Layout from './layout';
+import Popover from './popover';
+import Row from './row';
+import Sider from './sider';
 import Tabs from './tabs';
-import TabsNav from './tabs-nav';
 import TabsBody from './tabs-body';
 import TabsItem from './tabs-item';
+import TabsNav from './tabs-nav';
 import TabsPane from './tabs-pane';
-import Popover from './popover'
+import Toast from './toast';
+import Vue from 'vue';
+import plugin from './plugin';
 Vue.component('g-button', Button);
-Vue.component('g-icon', Icon);
 Vue.component('g-button-group', ButtonGroup);
-Vue.component('g-input', Input);
-Vue.component('g-row', Row);
+Vue.component('g-cascader',Cascader);
 Vue.component('g-col', Col);
-Vue.component('g-layout', Layout);
-Vue.component('g-header', Header);
-Vue.component('g-sider', Sider);
 Vue.component('g-content', Content);
 Vue.component('g-footer', Footer);
-Vue.component('g-toast', Toast);
+Vue.component('g-header', Header);
+Vue.component('g-icon', Icon);
+Vue.component('g-input', Input);
+Vue.component('g-layout', Layout);
+Vue.component('g-popover', Popover);
+Vue.component('g-row', Row);
+Vue.component('g-sider', Sider);
 Vue.component('g-tabs', Tabs);
-Vue.component('g-tabs-nav', TabsNav);
 Vue.component('g-tabs-body', TabsBody);
 Vue.component('g-tabs-item', TabsItem);
+Vue.component('g-tabs-nav', TabsNav);
 Vue.component('g-tabs-pane', TabsPane);
-Vue.component('g-popover', Popover);
+Vue.component('g-toast', Toast);
 Vue.use(plugin);
 new Vue({
     el: '#app',
     data: {
-        selectedTab: 'taiwan'
+        source:[{
+            name: "浙江",
+            children:[
+                {name: "杭州",
+                    children:[
+                        {name:"上城"},
+                        {name:"下城"},
+                        {name:"江干"}
+                    ]
+                },
+                {name: "嘉兴",
+                    children:[
+                        {name:"南湖"},
+                        {name:"秀杰"},
+                        {name:"嘉善"}
+                    ]
+                }
+            ]
+        },{
+            name: "福建",
+            children: [
+                {name: "福州",
+                    children:[
+                        {name: "鼓楼"},
+                        {name: "台江"},
+                        {name: "仓山"}
+                    ]
+                }
+            ]
+        }]
     },
     mounted() {},
     methods: {
-        yyy(){
-            debugger
-            console.log('yyy')
-        }
     }
 });
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,15 +1,41 @@
 <template>
     <div class="cascader">
-        <slot></slot>
+        <div class="trigger" @click="popoverVisible=!popoverVisible">
+
+        </div>
+        <div class="popover" v-if="popoverVisible">
+            <cascader-item :items="source"></cascader-item>
+        </div>
     </div>
 </template>
 <script>
+import cascaderItem from './cascader-item';
 export default {
-    name: "jcCasvader"
+    name: "jcCasvader",
+    props:{
+        source:{
+            type:Array
+        }
+    },
+    data(){
+        return {
+            popoverVisible: false
+        }
+    },
+    components:{cascaderItem}
 }
 </script>
 <style lang="scss" scoped>
     .cascader{
-        
+        .trigger{
+            border: 1px solid red;
+            height: 32px;
+            width: 100px;
+        }
+        .popover{
+            border: 1px solid red;
+            height: 200px;
+            display: flex;
+        }
     }
 </style>
